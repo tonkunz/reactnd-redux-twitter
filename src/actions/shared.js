573e32a0610fc49a27adabfcae20be78f7abe5b4
Everton Kunz <ehokeverton@gmail.com>
2019-02-01 20:40:03 -0200

    Primeiras ações

---
+++ src/actions/shared.js
@@ -0,0 +1,18 @@
+import { getInitialData } from '../utils/api'
+import {
+  receiveTweets
+} from './tweets'
+import {
+  receiveUsers
+} from './users'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, tweets}) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveTweets(tweets))
+      })
+      .catch(() => alert('Erro ao carregar dados iniciais'))
+  }
+}
