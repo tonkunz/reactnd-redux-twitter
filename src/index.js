85fed81fa1ee5d38d9696b1b01e9abc4f34a5835
Everton Kunz <ehokeverton@gmail.com>
2019-02-01 22:50:31 -0200

    Store criada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+//External dependences
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//Our dependences
+import App from './components/App'
+import reducer from './reducers'
+
+//style
+import './index.css'
+
+//Creating the Redux Store
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
